b31f4412090a9fa4d62e560efc734d819b765efc
Lorenzo Delana <lorenzo.delana@gmail.com>
2021-05-23 11:57:11 +0200

    postprocess doc to remove updated on to reduce changes

--- build-doc.sh
+++ build-doc.sh
@@ -11,3 +11,5 @@ cd "$exdir"/data
 rm -fr "$exdir"/data/api
 mkdir -p "$exdir"/data/api
 doxybook2 -i "$exdir"/xml -o "$exdir"/data/api -c "$exdir"/data/doxybook.json
+
+find "$exdir"/data/api  -type f -exec sed -i 's/Updated on.*//g' "{}" \;
